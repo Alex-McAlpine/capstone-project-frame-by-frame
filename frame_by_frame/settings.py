03880a64b4f698191a44718a61b2116d42c87660
Alex-McAlpine <alexmcalpine7@hotmail.com>
2025-08-01 14:29:22 +0100

    UPDATED: Secret key fix

--- frame_by_frame/settings.py
+++ frame_by_frame/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g3p^w6j2hv)&5_%b@azvjx96*e!xwe@kj-_3o+6z*l0k*kxs$*'
+SECRET_KEY =os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
